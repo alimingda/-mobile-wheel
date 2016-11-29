69544cef09bfc959ad1bb27fd6b6a99eac441510
alimingda <1069330664@qq.com>
2016-11-30 01:15:52 +0800

    chumo1

---
+++ chumo/js/index.js
@@ -0,0 +1,39 @@
+$(function(){
+    $("#fullpage").mousedown(function(e){
+        e.preventDefault();
+    });
+    $("#fullpage").mousemove(function(e){
+        e.preventDefault();
+    });
+    var ht=$(window).height();
+    var num=0;
+    var flag=true;
+    touch.on("body","swipeup","#fullpage",function(){
+        if(!flag){
+            return;
+        }
+        num++;
+        if(num>=$("section").length){
+            num=$("section").length-1;
+            return
+        }
+        flag=false;
+        $("#fullpage").css({marginTop:-num*ht});
+    })
+    touch.on("body","swipedown","#fullpage",function(){
+        if(!flag){
+            return;
+        }
+        num--;
+        if(num==-1){
+            num=0;
+            return
+        }
+        flag=false;
+        $("#fullpage").css({marginTop:-num*ht});
+    });
+    $("#fullpage")[0].addEventListener("webkitTransitionEnd",function(){
+        flag=true;
+    })
+
+});
